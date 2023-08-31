a0fecb5da46bb229a4e0ef4d60c7d15c0d0c745c
Ihor <ihor.cherednychenko@nure.ua>
2023-08-31 20:57:29 +0300

    Add Task5 change solution of Day1

--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -1,9 +1,16 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args) {
-        for (int year = 1980; year <= 2020; year += 4) {
-            System.out.println("Олимпиада " + year + " года");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first date: ");
+        int firstDate = scanner.nextInt();
+        System.out.print("Enter last date: ");
+        int lastDate = scanner.nextInt();
+        for (int i = firstDate; i <= lastDate; i += 4) {
+            System.out.println("Олимпиада " + i + " года");
         }
     }
 }

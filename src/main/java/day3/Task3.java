8922b18c6186e9c9b7c3b10f563faedaa8dfdd3c
Ihor <ihor.cherednychenko@nure.ua>
2023-09-02 19:45:36 +0300

    Add Task3 solution of Day3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
 
+        while (counter < 5) {
+            double dividend = scanner.nextDouble();
+            double divisor = scanner.nextDouble();
+
+            switch ((int) divisor) {
+                case 0:
+                    System.out.println("Деление на 0");
+                    break;
+                default:
+                    System.out.println(dividend / divisor);
+            }
+            counter++;
+        }
     }
 }

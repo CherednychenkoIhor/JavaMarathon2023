438f9c0e8d256ab4c98db0804b1fea3876d446ce
Ihor <ihor.cherednychenko@nure.ua>
2023-09-03 12:35:52 +0300

    Add Task1 solution of Day4

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int counter = 0;
 
         while (counter < 5) {
             double dividend = scanner.nextDouble();
             double divisor = scanner.nextDouble();
 
-            switch ((int) divisor) {
-                case 0:
-                    System.out.println("Деление на 0");
-                    break;
-                default:
-                    System.out.println(dividend / divisor);
-            }
             counter++;
+
+            if (divisor == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            System.out.println(dividend / divisor);
         }
     }
 }
--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,7 +1,44 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int sum = 0;
+        int counterMoreThanEight = 0;
+        int counterEven = 0;
+        int counterOdd = 0;
+        int counterNumberOne = 0;
+        int n = scanner.nextInt();
+        int[] mas = new int[n];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = random.nextInt(11);
+        }
 
+        for (int arr : mas) {
+            sum += arr;
+            if (arr > 8) {
+                counterMoreThanEight++;
+            } else if (arr == 1) {
+                counterNumberOne++;
+            }
+            if (arr % 2 == 0) {
+                counterEven++;
+            } else {
+                counterOdd++;
+            }
+        }
+
+        System.out.println(Arrays.toString(mas));
+        System.out.println("Длина массива: " + mas.length);
+        System.out.println("Количество чисел больше 8: " + counterMoreThanEight);
+        System.out.println("Количество чисел равных 1: " + counterNumberOne);
+        System.out.println("Количество четных чисел: " + counterEven);
+        System.out.println("Количество нечетных чисел: " + counterOdd);
+        System.out.println("Сумма всех элементов массива: " + sum);
     }
 }

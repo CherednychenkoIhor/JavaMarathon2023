04c2774dcf2a5a3de00fdb8b413b3a37bda9ca8f
Ihor <ihor.cherednychenko@nure.ua>
2023-09-03 14:14:32 +0300

    Add Task2-3 solution of Day4

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,42 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int[] mas = new int[100];
+        int largestArrayElement;
+        int smallestArrayElement;
+        int countLastZeroElement = 0;
+        int sumLastZeroElement = 0;
+        int[] testMass = new int[mas.length];
 
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = random.nextInt(10000);
+        }
+
+        largestArrayElement = mas[0];
+        smallestArrayElement = mas[0];
+
+        for (int arr : mas) {
+            if (arr > largestArrayElement) {
+                largestArrayElement = arr;
+            } else if (arr < smallestArrayElement) {
+                smallestArrayElement = arr;
+            } else if (arr % 10 == 0 || arr % 100 == 0 || arr % 1000 == 0) {
+                testMass[countLastZeroElement] = arr;
+                sumLastZeroElement += arr;
+                countLastZeroElement++;
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+        System.out.println("Наибольший элемент массива: " + largestArrayElement);
+        System.out.println("Наименьший элемент массива: " + smallestArrayElement);
+        System.out.println("Количество элементов массива, оканчивающихся на 0: " + countLastZeroElement);
+        System.out.println("Сумма элементов массива, оканчивающихся на 0: " + sumLastZeroElement);
+
+        System.out.println(Arrays.toString(testMass));
     }
 }
--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,39 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task3 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int m = 12;
+        int n = 8;
+        int[][] matrix = new int[m][n];
+        int[] sumMax = new int[m];
+        int maxElem;
+        int stringIndex = 0;
 
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = random.nextInt(50);
+                sumMax[i] += matrix[i][j];
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("    | Sum = " + sumMax[i]);
+        }
+
+        maxElem = sumMax[0];
+
+        for (int i = 0; i < sumMax.length; i++) {
+            if (sumMax[i] > maxElem){
+                maxElem = sumMax[i];
+            }
+            if (sumMax[i] == maxElem) {
+                stringIndex = i;
+            }
+        }
+
+        System.out.println("\nMax sum = " + maxElem);
+        System.out.println("Индекс строки, сумма чисел в которой максимальна: " + stringIndex);
     }
 }

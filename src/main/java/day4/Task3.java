17e21f6a58890cedcd4f6edd7675d2bec19e68b1
Ihor <ihor.cherednychenko@nure.ua>
2023-09-03 14:14:32 +0300

    Add Task4 solution of Day4

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,31 @@
 package day4;
 
+import java.util.Random;
+
 public class Task3 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int m = 12;
+        int n = 8;
+        int[][] matrix = new int[m][n];
+        int maxElem = 0;
+        int stringIndex = 0;
 
+        for (int i = 0; i < matrix.length; i++) {
+            int sumMax = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = random.nextInt(50);
+                sumMax += matrix[i][j];
+                System.out.print(matrix[i][j] + " ");
+            }
+                if (sumMax > maxElem) {
+                    maxElem = sumMax;
+                    stringIndex = i;
+                }
+            System.out.println("   | Sum = " + sumMax);
+        }
+
+        System.out.println("\nMax sum = " + maxElem);
+        System.out.println("Индекс строки, сумма чисел в которой максимальна: " + stringIndex);
     }
 }
--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,30 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task4 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int[] mas = new int[100];
 
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = random.nextInt(10000);
+        }
+        System.out.println(Arrays.toString(mas));
+        int maxSum = 0;
+        int maxSumIdx = 0;
+        for (int i = 0; i < mas.length - 2; i++) {
+            int sum = 0;
+            for (int j = 0; j < i + 3; j++) {
+                sum += mas[j];
+            }
+            if (sum > maxSum){
+                maxSum = sum;
+                maxSumIdx = i;
+            }
+        }
+        System.out.println(maxSum);
+        System.out.println(maxSumIdx);
     }
 }

7d1407ee2ed03841b6ff704b05e805ed56e5ef89
Ihor <ihor.cherednychenko@nure.ua>
2023-09-04 15:13:59 +0300

    Add Task1,2 solution of Day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,12 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setModel("Mitsubishi");
+        car.setColor("Blue");
+        car.setYear(2006);
+        System.out.println("Car model: " + car.getModel());
+        System.out.println("Car color: " + car.getColor());
+        System.out.println("Car year manufacture: " + car.getYear());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -2,6 +2,9 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike motorbike = new Motorbike(2022, "Red", "Kawasaki Ninja");
+        System.out.println("Motorbike year manufacture: " + motorbike.getYear());
+        System.out.println("Motorbike color: " + motorbike.getColor());
+        System.out.println("Motorbike model: " + motorbike.getModel());
     }
 }

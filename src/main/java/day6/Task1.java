81aab54b2d7b74aad4ca97f87975b4876fbfc05b
Ihor <ihor.cherednychenko@nure.ua>
2023-09-04 18:38:55 +0300

    Add Task1 solution of Day6

--- src/main/java/day6/Task1.java
+++ src/main/java/day6/Task1.java
@@ -2,6 +2,7 @@ package day6;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        Motorbike motorbike = new Motorbike("Kawasaki", "Red", 2020);
     }
 }

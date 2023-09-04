18c10093a0c2aa30c7632b4e8b026f22a587dcf5
Ihor <ihor.cherednychenko@nure.ua>
2023-09-04 19:00:28 +0300

    Add Task2 solution of Day6

--- src/main/java/day6/Task2.java
+++ src/main/java/day6/Task2.java
@@ -2,6 +2,11 @@ package day6;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane("Boing", 2020, 20, 10);
+        airplane.setYear(1999);
+        airplane.setLength(18);
+        airplane.fillUp(1000);
+        airplane.fillUp(1500);
+        airplane.info();
     }
 }

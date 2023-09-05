e6f18f9923c5cbcf66a593adb458c5fee869b163
Ihor <ihor.cherednychenko@nure.ua>
2023-09-05 17:46:39 +0300

    Add Task1.java solution of Day7

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,8 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Airplane airplane1 = new Airplane("Boing", 2020,25, 244);
+        Airplane airplane2 = new Airplane("TY", 1990,45, 100);
+        Airplane.compareAirplanes(airplane1, airplane2);
     }
 }

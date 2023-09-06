eb0f3ed3f15740202e115a460cb469c7475fafb3
Ihor <ihor.cherednychenko@nure.ua>
2023-09-06 18:01:51 +0300

    Add Task1 solution of Day8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,18 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+//        String numbers = "";
+//
+//        for (int i = 0; i <= 20000; i++) {
+//            numbers += i + " ";
+//        }
+//
+//        System.out.println(numbers);
 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
     }
 }

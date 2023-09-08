fcb0db024e57d615ff135930c1f316ec1feead8d
Ihor <ihor.cherednychenko@nure.ua>
2023-09-08 17:21:46 +0300

    Add Task1 solution of Day12

--- src/main/java/day12/Task1.java
+++ src/main/java/day12/Task1.java
@@ -1,7 +1,20 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        List<String> marks = new ArrayList<>();
+        marks.add("BMW");
+        marks.add("Lada");
+        marks.add("Mercedes-Benz");
+        marks.add("Volvo");
+        marks.add("Nissan");
+        System.out.println(marks);
+        marks.add(2,"Mitsubishi");
+        System.out.println(marks);
+        marks.remove(0);
+        System.out.println(marks);
     }
 }

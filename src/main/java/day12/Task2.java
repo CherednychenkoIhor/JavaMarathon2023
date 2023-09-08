3ee6b8c9ebf86504189762a1ad10eb9b1b206ec8
Ihor <ihor.cherednychenko@nure.ua>
2023-09-08 17:29:59 +0300

    Add Task2 solution of Day12

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,7 +1,23 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2 {
     public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
 
+        for (int i = 0; i < 350; i++) {
+            if (i < 30) {
+                if (i % 2 == 0) {
+                    numbers.add(i);
+                }
+            }  else if (i >= 300) {
+                if (i % 2 == 0) {
+                    numbers.add(i);
+                }
+            }
+        }
+        System.out.println(numbers);
     }
 }

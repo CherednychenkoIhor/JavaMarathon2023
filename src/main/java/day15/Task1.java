f6e7a4b7cb24641bf16f07477efff94ff227ceda
Ihor <ihor.cherednychenko@nure.ua>
2023-09-10 17:02:05 +0300

    Add Task1 solution of Day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,29 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/main/resources/shoes.csv");
+        File file1 = new File("src/main/resources/missing_shoes.txt");
+
+        Scanner scanner = new Scanner(file);
+        PrintWriter pw = new PrintWriter(file1);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            String[] shoes = line.split(";");
+
+            if (Integer.parseInt(shoes[2]) == 0 ) {
+                pw.println(shoes[0] + ", " + shoes[1] + ", " + shoes[2]);
+            }
+            System.out.println(line);
+        }
+        pw.close();
+        scanner.close();
     }
 }

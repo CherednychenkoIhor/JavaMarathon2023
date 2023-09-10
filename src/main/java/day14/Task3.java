6fc59bb4a756534633c9ba5f43e3ffa68ca7445b
Ihor <ihor.cherednychenko@nure.ua>
2023-09-10 16:13:09 +0300

    Add Task3 solution of Day14

--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,41 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+//        System.out.println(parseFileToObjList());
+    }
 
+    public static List<Person> parseFileToObjList(File file) {
+//        File file = new File("people.txt");
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] personName = line.split(" ");
+
+                if (Integer.parseInt(personName[1]) < 0 ) {
+                    throw new IllegalArgumentException();
+                }
+
+                Person person = new Person(personName[0], Integer.parseInt(personName[1]));
+
+                people.add(person);
+            }
+            return people;
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return null;
     }
 }

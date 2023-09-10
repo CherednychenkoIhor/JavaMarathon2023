2ef8ead9af4fec63930a30b1c8377cc4fa3c6c37
Ihor <ihor.cherednychenko@nure.ua>
2023-09-10 14:17:25 +0300

    Add Task1 solution of Day13

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,17 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("Ihor");
+        User user2 = new User("Bob");
 
+        MessageDatabase.sendMessage(user2, user1, "Hi, Ihor");
+        MessageDatabase.sendMessage(user2, user1, "Are U ok?");
+        MessageDatabase.sendMessage(user2, user1, "Mb go d2?");
+        MessageDatabase.sendMessage(user1, user2, "Hi, Bob");
+        MessageDatabase.sendMessage(user1, user2, "It`s ok, bro");
+        MessageDatabase.sendMessage(user1, user2, "Let`s do this");
+        MessageDatabase.sendMessage(user2, user1, "Ok, let`s go");
+
+        MessageDatabase.showDialog(user1, user2);
     }
 }

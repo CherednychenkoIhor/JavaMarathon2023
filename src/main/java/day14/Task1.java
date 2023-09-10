76f7f83b251695b34308122cc6f761e2fc00d9c8
Ihor <ihor.cherednychenko@nure.ua>
2023-09-10 15:18:48 +0300

    Add Task1 solution of Day14

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,38 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        printSumDigits(new File("test.txt"));
+    }
 
+    public static void printSumDigits(File file) {
+
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            String line = scanner.nextLine();
+            String[] numberString = line.split(" ");
+
+            if (numberString.length != 10)
+                throw new IllegalArgumentException();
+
+            int sum = 0;
+
+            for (String number : numberString) {
+                sum += Integer.parseInt(number);
+            }
+
+            System.out.println(sum);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }

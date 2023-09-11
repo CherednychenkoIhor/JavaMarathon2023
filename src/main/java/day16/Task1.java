98889f2b866f3f915c28f3be6f64bc73dbedebfa
Ihor <ihor.cherednychenko@nure.ua>
2023-09-11 17:38:59 +0300

    Add Task1 solution of Day16

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,33 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        printResult(new File("test.txt"));
+    }
 
+    public static void printResult (File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+
+            String line = scanner.nextLine();
+            String[] numberString = line.split(" ");
+
+            double average = 0d;
+
+            for (String averageString : numberString) {
+                average += Double.parseDouble(averageString);
+            }
+
+            average /= numberString.length;
+
+            System.out.printf(average + " --> %.3f", average);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
